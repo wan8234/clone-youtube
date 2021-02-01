a464dbc352b267ef540b9e9091f4512d16db79b3
wan8234 <wan8234@naver.com>
2021-02-01 15:22:40 +0900

    make video_list and video_item

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import VideoList from "./components/video_list/video_list";
 import "./app.css";
-import Navbar from "./components/navbar";
-import Video from "./components/video";
 
-class App extends Component {
-    state = {
-        item: null,
-    };
-    handleSearch = (item) => {
-        this.setState({ item: item });
-    };
+function App() {
+    const [videos, setVideos] = useState([]);
 
-    render() {
-        return (
-            <>
-                <Navbar onSearch={this.handleSearch} />
-                <Video item={this.state.item} />
-            </>
-        );
-    }
+    useEffect(() => {
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow",
+        };
+
+        fetch(
+            "https://www.googleapis.com/youtube/v3/videos?part=snippet&maxResults=25&chart=mostPopular&key=AIzaSyCS8TEpiToeD1FyhU_13__Cbq7e41TjGt8",
+            requestOptions
+        )
+            .then((response) => response.json())
+            .then((result) => setVideos(result.items))
+            .catch((error) => console.log("error", error));
+    }, []);
+
+    return <VideoList videos={videos} />;
 }
-
 export default App;
