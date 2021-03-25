6f3f417c51edeea8862dcce13f33619d3cdfae4a
wan8234 <wan8234@naver.com>
2021-03-26 00:17:55 +0900

    making player

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from "react";
 import VideoList from "./components/video_list/video_list";
 import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
     const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+    const selectVideo = (video) => {
+        setSelectedVideo(video);
+    };
     const search = (query) => {
         youtube
             .search(query) //
@@ -19,7 +25,20 @@ function App({ youtube }) {
     return (
         <div className={styles.app}>
             <SearchHeader onSearch={search} />
-            <VideoList videos={videos} />
+            <section className={styles.content}>
+                {selectedVideo && (
+                    <div className={styles.detail}>
+                        <VideoDetail video={selectedVideo} />
+                    </div>
+                )}
+                <div className={styles.list}>
+                    <VideoList
+                        videos={videos}
+                        onVideoClick={selectVideo}
+                        display={selectedVideo ? "list" : "grid"}
+                    />
+                </div>
+            </section>
         </div>
     );
 }
